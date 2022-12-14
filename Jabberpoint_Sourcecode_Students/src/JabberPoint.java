4783b2bdeebdf2eae4e9712f15d609e94a4954a4
Noizeisdead <noizee829@gmail.com>
2022-12-14 15:17:35 +0100

    Move initializing of styles to Jabberpoint.java

--- Jabberpoint_Sourcecode_Students/src/JabberPoint.java
+++ Jabberpoint_Sourcecode_Students/src/JabberPoint.java
@@ -1,5 +1,6 @@
 import javax.swing.JOptionPane;
 
+import java.awt.*;
 import java.io.IOException;
 
 /** JabberPoint Main Program
@@ -17,15 +18,22 @@ import java.io.IOException;
  */
 
 public class JabberPoint {
+	private static Style[] styles; // de styles
 	protected static final String IOERR = "IO Error: ";
 	protected static final String JABERR = "Jabberpoint Error ";
 	protected static final String JABVERSION = "Jabberpoint 1.6 - OU version";
 
 	/** The main program */
 	public static void main(String[] argv) {
-		
-		Style.createStyles();
+
+		styles = new Style[5];
+ 		styles[0] = new Style(0, Color.red,   48, 20);	// style item-level 0
+ 		styles[1] = new Style(20, Color.blue,  40, 10);	// style item-level 1
+ 		styles[2] = new Style(50, Color.black, 36, 10);	// style item-level 2
+ 		styles[3] = new Style(70, Color.black, 30, 10);	// style item-level 3
+ 		styles[4] = new Style(90, Color.black, 24, 10);	// style item-level 4
 		Presentation presentation = new Presentation();
+
 		new SlideViewerFrame(JABVERSION, presentation);
 		try {
 			if (argv.length == 0) { //a demo presentation
@@ -40,4 +48,12 @@ public class JabberPoint {
 					JOptionPane.ERROR_MESSAGE);
 		}
 	}
+
+	public static Style getStyle(int level) {
+		if (level >= styles.length) {
+			level = styles.length - 1;
+		}
+		return styles[level];
+	}
+
 }
